aa7ad62ab4c0a62b6387676c846073a7904875bd
= <=>
2024-07-31 09:44:29 +0700

    update

--- src/main/java/com/example/qlusers/utils/student_jdbc.sql
+++ src/main/java/com/example/qlusers/utils/student_jdbc.sql
@@ -10,3 +10,51 @@ create table Students
     status   bit
 
 );
+
+# stored procedure lấy ra tất cả students
+delimiter //
+create procedure PROC_GET_ALL_USER()
+begin
+select *
+from students;
+end//
+
+# stored procedure tìm ra thông qua id
+delimiter //
+create procedure PROC_GET_BY_ID(idFind int)
+begin
+select * from students where id = idFind;
+end //
+# stored procedure thêm mới
+delimiter //
+create procedure PROC_ADD_STUDENT(newFullName varchar(100), newEmail varchar(100), newAddress varchar(100),
+                                  newPhone varchar(11), newStatus bit)
+begin
+insert into students (fullName, email, address, phone, status)
+values (newFullName, newEmail, newAddress, newPhone, newStatus);
+end //
+# stored procedure cập nhật
+delimiter //
+create procedure PROC_UPDATE_STUDENT(newFullName varchar(100), newEmail varchar(100), newAddress varchar(100),
+                                     newPhone varchar(11), newStatus bit, idUpdate int)
+begin
+update students
+set fullName = newFullName,
+    email    = newEmail,
+    address  = newAddress,
+    phone    = newPhone,
+    status   = newStatus
+where id = idUpdate;
+end //
+# stored procedure xóa
+delimiter //
+create procedure PROC_DELETE_STUDENT(idDelete int)
+begin
+delete from students where id = idDelete;
+end //
+# stored procedure tìm kiếm
+delimiter //
+create procedure PROC_SEARCH_BY_NAME(keyword varchar(255))
+begin
+select * from students where fullName like concat('%', keyword, '%');
+end //
